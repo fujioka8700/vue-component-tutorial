a305eaa26cff74d610c2a535fe5d2bee7b5d027a
fujioka8700 <fujioka8700@gmail.com>
2021-09-21 21:49:44 +0900

    [add] コンポーネントの作成、登録、使用方法の理解

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 
 // Components
 import Hello from './components/Hello.vue'
+import AnchorLink from './components/AnchorLink.vue'
 
 new Vue({
   el: '#app',
   components: {
-    hello: Hello
+    hello: Hello,
+    AnchorLink
   }
 })
